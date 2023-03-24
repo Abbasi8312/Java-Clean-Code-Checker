811013f9748c153ac90c74e00694e8af6ff5a576
Ali <abbasi8312@gmail.com>
2023-03-24 13:26:06 +0430

    Modify code - Add method statement - Remove space checkers

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -3,10 +3,17 @@ package ir.ac.kntu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    private static int lineCounter;
+
+    private static int indentation;
+
+    private static String line;
 
     public static void main(String[] args) {
         BufferedReader reader;
@@ -21,30 +28,30 @@ public class Main {
     }
 
     private static void cleanCodeTest(BufferedReader reader) throws IOException {
-        String line = reader.readLine();
-        int lineCounter = 1;
-        int indentation = 0;
+        line = reader.readLine();
+        lineCounter = 1;
         int offset = 0;
         while (line != null) {
-            Matcher packageMatcher = getMatcher("package( +)\\S+( *);", line);
-            Matcher importMatcher = getMatcher("import( +)\\S+( *);", line);
-            Matcher classMatcher = getMatcher("public( +)class( +)(\\S+)( *)\\{", line);
-            Matcher bracesMatcher = getMatcher("}", line);
+            Map<String, Matcher> matcherMap = new HashMap<>();
+            fillMatcherMap(matcherMap);
             if (offset != 0) {
                 System.out.println(lineCounter + ": Multiple statements in one line!");
             }
-            if (packageMatcher.find(offset)) {
-                packageTest(lineCounter, indentation, packageMatcher);
-                offset = getOffset(packageMatcher);
-            } else if (importMatcher.find(offset)) {
-                importTest(lineCounter, indentation, importMatcher);
-                offset = getOffset(importMatcher);
-            } else if (classMatcher.find(offset)) {
-                indentation = classTest(lineCounter, indentation, classMatcher);
-                offset = getOffset(classMatcher);
-            } else if (bracesMatcher.find(offset)) {
-                indentation = bracesTest(lineCounter, indentation, bracesMatcher);
-                offset = getOffset(bracesMatcher);
+            if (matcherMap.get("package").find(offset)) {
+                packageTest(matcherMap.get("package"));
+                offset = getOffset(matcherMap.get("package"));
+            } else if (matcherMap.get("import").find(offset)) {
+                importTest(matcherMap.get("import"));
+                offset = getOffset(matcherMap.get("import"));
+            } else if (matcherMap.get("class").find(offset)) {
+                classTest(matcherMap.get("class"));
+                offset = getOffset(matcherMap.get("class"));
+            } else if (matcherMap.get("method").find(offset)) {
+                methodTest(matcherMap.get("method"));
+                offset = getOffset(matcherMap.get("method"));
+            } else if (matcherMap.get("braces").find(offset)) {
+                bracesTest(matcherMap.get("braces"));
+                offset = getOffset(matcherMap.get("braces"));
             } else {
                 offset = 0;
             }
@@ -55,48 +62,62 @@ public class Main {
         }
     }
 
-    private static int bracesTest(int lineCounter, int indentation, Matcher braceMatcher) {
-        indentation--;
-        indentationCheck(lineCounter, indentation, braceMatcher);
-        return indentation;
+    private static void fillMatcherMap(Map<String, Matcher> matcherMap) {
+        matcherMap.put("package", getMatcher("package +\\S+ *;"));
+        matcherMap.put("import", getMatcher("import +\\S+ *;"));
+        matcherMap.put("class", getMatcher("public +class +(?<name>\\S+) *\\{"));
+        matcherMap.put("braces", getMatcher("}"));
+        matcherMap.put("method",
+                getMatcher("public +static +(?<type>\\S+) +(?<name>\\S+) *\\((?<parameters>.*?)\\) *\\{"));
     }
 
-    private static int classTest(int lineCounter, int indentation, Matcher classMatcher) {
-        indentationCheck(lineCounter, indentation, classMatcher);
-        indentation++;
-        if (!classMatcher.group(2).equals(" ") || !classMatcher.group(3).equals(" ") ||
-                !classMatcher.group(5).equals(" ")) {
-            System.out.println(lineCounter + ": Wrong space count!");
-        }
-        if (!classMatcher.group(4).matches("^[A-Z][a-zA-Z]*$")) {
-            System.out.println(lineCounter + ": Class name should be UpperCamelCase!");
-        }
-        return indentation;
-    }
-
-    private static void importTest(int lineCounter, int indentation, Matcher importMatcher) {
-        indentationCheck(lineCounter, indentation, importMatcher);
-        if (!importMatcher.group(2).equals(" ") || !importMatcher.group(3).equals("")) {
-            System.out.println(lineCounter + ": Wrong space count!");
-        }
-    }
-
-    private static void packageTest(int lineCounter, int indentation, Matcher packageMatcher) {
-        indentationCheck(lineCounter, indentation, packageMatcher);
-        if (lineCounter != 1) {
-            System.out.println(lineCounter + ": The \"package\" statement should be the first line of code!");
-        }
-        if (!packageMatcher.group(2).equals(" ") || !packageMatcher.group(3).equals("")) {
-            System.out.println(lineCounter + ": Wrong space count!");
-        }
-    }
-
-    private static Matcher getMatcher(String regex, String line) {
+    private static Matcher getMatcher(String regex) {
         return Pattern.compile("(?<indentation> *)" + regex + " *(?<offset>.*?)(?://.*)?$").matcher(line);
     }
 
-    private static void indentationCheck(int lineCounter, int indentation, Matcher matcher) {
-        if (matcher.group("indentation").length() != indentation * 4) {
+    private static void methodTest(Matcher methodMatcher) {
+        indentationCheck(methodMatcher);
+        indentation += 4;
+        if (!methodMatcher.group("name").matches("^[a-z][a-zA-Z0-9]+$")) {
+            System.out.println(
+                    lineCounter + ": The method name should be in lowerCamelCase and must have at least 2 characters.");
+        }
+        String parameters = methodMatcher.group("parameters");
+        Matcher parameterMatcher = Pattern.compile("\\s*(?<type>\\S+)\\s+(?<name>[^ ,]+)").matcher(parameters);
+        while (parameterMatcher.find()) {
+            if (!parameterMatcher.group("name").matches("^[a-z][a-zA-Z0-9]+$")) {
+                System.out.println(lineCounter +
+                        ": The variable name should be in lowerCamelCase and must have at least 2 characters.");
+            }
+        }
+    }
+
+    private static void bracesTest(Matcher bracesMatcher) {
+        indentation -= 4;
+        indentationCheck(bracesMatcher);
+    }
+
+    private static void classTest(Matcher classMatcher) {
+        indentationCheck(classMatcher);
+        indentation += 4;
+        if (!classMatcher.group("name").matches("^[A-Z][a-zA-Z0-9]*$")) {
+            System.out.println(lineCounter + ": The class name should be in UpperCamelCase.");
+        }
+    }
+
+    private static void importTest(Matcher importMatcher) {
+        indentationCheck(importMatcher);
+    }
+
+    private static void packageTest(Matcher packageMatcher) {
+        indentationCheck(packageMatcher);
+        if (lineCounter != 1) {
+            System.out.println(lineCounter + ": The \"package\" statement should be the first line of code!");
+        }
+    }
+
+    private static void indentationCheck(Matcher matcher) {
+        if (matcher.group("indentation").length() != indentation) {
             System.out.println(lineCounter + ": Wrong indentation!");
         }
     }
