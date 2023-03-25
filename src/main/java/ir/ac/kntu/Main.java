061876dacc081716fe3520029f9f93dc00dc736f
Ali <abbasi8312@gmail.com>
2023-03-25 20:36:19 +0430

    Add new space convention checker - Reformat output for better readability

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -35,7 +35,7 @@ public class Main {
             Map<String, Matcher> matcherMap = new HashMap<>();
             fillMatcherMap(matcherMap);
             if (offset != 0) {
-                System.out.println(lineCounter + ": Multiple statements in one line!");
+                System.out.printf("%3d| Multiple statements in one line!\n", lineCounter);
             }
             if (matcherMap.get("package").find(offset)) {
                 packageTest(matcherMap.get("package"));
@@ -56,6 +56,7 @@ public class Main {
                 offset = 0;
             }
             if (offset == 0) {
+                spaceCheck();
                 lineCounter++;
                 line = reader.readLine();
             }
@@ -79,15 +80,16 @@ public class Main {
         indentationCheck(methodMatcher);
         indentation += 4;
         if (!methodMatcher.group("name").matches("^[a-z][a-zA-Z0-9]+$")) {
-            System.out.println(
-                    lineCounter + ": The method name should be in lowerCamelCase and must have at least 2 characters.");
+            System.out.printf("%3d| The method name should be in lowerCamelCase and must have at least 2 characters.\n",
+                    lineCounter);
         }
         String parameters = methodMatcher.group("parameters");
         Matcher parameterMatcher = Pattern.compile("\\s*(?<type>\\S+)\\s+(?<name>[^ ,]+)").matcher(parameters);
         while (parameterMatcher.find()) {
             if (!parameterMatcher.group("name").matches("^[a-z][a-zA-Z0-9]+$")) {
-                System.out.println(lineCounter +
-                        ": The variable name should be in lowerCamelCase and must have at least 2 characters.");
+                System.out.printf(
+                        "%3d| The variable name should be in lowerCamelCase and must have at least 2 characters.\n",
+                        lineCounter);
             }
         }
     }
@@ -101,7 +103,7 @@ public class Main {
         indentationCheck(classMatcher);
         indentation += 4;
         if (!classMatcher.group("name").matches("^[A-Z][a-zA-Z0-9]*$")) {
-            System.out.println(lineCounter + ": The class name should be in UpperCamelCase.");
+            System.out.printf("%3d| The class name should be in UpperCamelCase.\n", lineCounter);
         }
     }
 
@@ -112,15 +114,109 @@ public class Main {
     private static void packageTest(Matcher packageMatcher) {
         indentationCheck(packageMatcher);
         if (lineCounter != 1) {
-            System.out.println(lineCounter + ": The \"package\" statement should be the first line of code!");
+            System.out.printf("%3d| The \"package\" statement should be the first line of code!\n", lineCounter);
         }
     }
 
     private static void indentationCheck(Matcher matcher) {
         if (matcher.group("indentation").length() != indentation) {
-            System.out.println(lineCounter + ": Wrong indentation!");
+            System.out.printf("%3d| Wrong indentation!\n", lineCounter);
         }
     }
+
+    private static String removeComments(String line) {
+        return line.replaceAll("//.*", "").trim();
+    }
+
+    private static String removeStringLiterals(String line) {
+        return line.replaceAll("\"(?:\\\\.|[^\"])*[^\\\\]\"", "\"\"").trim();
+    }
+
+    private static void checkOperatorSpacing(String line) {
+        Matcher spaceMatcher = Pattern.compile("(\\S*?)(\\s*)" +
+                "(\\|\\||\\+\\+|\\+=|\\*=|\\|=|&&|==|!=|<=|>=|--|-=|/=|%=|&=|^=|[\\[\\]\\-;:(){,=+*/<>^%&|])" +
+                "(\\s*)([^ \\[\\]\\-:(){,=+*/<>^%&|]*)").matcher(line);
+        int index = 0;
+        while (spaceMatcher.find(index)) {
+            checkOperatorSpacing(line, spaceMatcher);
+            index = spaceMatcher.end(3);
+        }
+    }
+
+    private static void checkOperatorSpacing(String line, Matcher spaceMatcher) {
+        switch (spaceMatcher.group(3)) {
+            case "++", "--" -> {
+                if (spaceMatcher.group(5).matches("[a-zA-Z0-9]+")) {
+                    edgeSpace(spaceMatcher, -1, 0);
+                } else {
+                    edgeSpace(spaceMatcher, 0, -1);
+                }
+            }
+            case ":", ";" -> edgeSpace(spaceMatcher, 0, -1);
+            case "[" -> edgeSpace(spaceMatcher, 0, 0);
+            case "]" -> {
+                if (spaceMatcher.group(5).matches("[a-zA-Z0-9]+")) {
+                    edgeSpace(spaceMatcher, 0, 1);
+                } else {
+                    edgeSpace(spaceMatcher, 0, 0);
+                }
+            }
+            case "(" -> {
+                if (spaceMatcher.group(1).matches("(if)|(else)|(for)|(while)|(switch)")) {
+                    edgeSpace(spaceMatcher, 1, 0);
+                } else if (spaceMatcher.group(1).matches("[a-zA-Z0-9.]+")) {
+                    edgeSpace(spaceMatcher, 0, 0);
+                } else {
+                    edgeSpace(spaceMatcher, 1, 0);
+                }
+            }
+            case ")" -> {
+                if (spaceMatcher.group(5).matches("[A-Za-z0-9]+")) {
+                    edgeSpace(spaceMatcher, 0, 1);
+                } else {
+                    edgeSpace(spaceMatcher, 0, -1);
+                }
+            }
+            case "{" -> {
+                if (line.charAt(spaceMatcher.start(2) - 1) != ']') {
+                    edgeSpace(spaceMatcher, 1, -1);
+                }
+            }
+            case "," -> edgeSpace(spaceMatcher, 0, 1);
+            default -> edgeSpace(spaceMatcher, 1, 1);
+        }
+    }
+
+    private static void checkWordSpacing(String line) {
+        Matcher spaceMatcher = Pattern.compile("([A-Za-z0-9]+)( +)([A-Za-z0-9]+)").matcher(line);
+        int index = 0;
+        while (spaceMatcher.find(index)) {
+            if (!spaceMatcher.group(2).equals(" ")) {
+                System.out.printf("%3d| There should be one space between \"%s\" and \"%s\"", lineCounter,
+                        spaceMatcher.group(1), spaceMatcher.group(3));
+            }
+            index = spaceMatcher.start(3);
+        }
+    }
+
+    public static void spaceCheck() {
+        String newLine = removeComments(line);
+        newLine = removeStringLiterals(newLine);
+        checkOperatorSpacing(newLine);
+        checkWordSpacing(newLine);
+    }
+
+    private static void edgeSpace(Matcher spaceMatcher, int leftCount, int rightCount) {
+        if (leftCount != -1 && !spaceMatcher.group(2).equals(leftCount == 0 ? "" : " ")) {
+            System.out.printf("%3d| There should be %s space before \"%s\"\n", lineCounter,
+                    leftCount == 0 ? "no" : "one", spaceMatcher.group(3));
+        }
+        if (rightCount != -1 && !spaceMatcher.group(4).equals(rightCount == 0 ? "" : " ")) {
+            System.out.printf("%3d| There should be %s space after \"%s\"\n", lineCounter,
+                    rightCount == 0 ? "no" : "one", spaceMatcher.group(3));
+        }
+    }
+
 
     private static int getOffset(Matcher matcher) {
         if (!matcher.group("offset").matches("")) {
